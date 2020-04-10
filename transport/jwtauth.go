4551c978fbee3520193d619e6cf0528b0dc05a51
Nimrod Gutman <nimrod.gutman@gmail.com>
2020-04-10 13:17:26 +0300

    Allow authenticating with development token
    For faster development cycles and as a preparation for multi-user

--- transport/jwtauth.go
+++ transport/jwtauth.go
@@ -3,11 +3,16 @@ package transport
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
+
 	"github.com/auth0-community/go-auth0"
 	"github.com/ngutman/kaboo-server-go/api/types"
 	"gopkg.in/square/go-jose.v2"
 	"gopkg.in/square/go-jose.v2/jwt"
-	"net/http"
 )
 
 // JWTAuthMiddleware handles Auth0 JWT validation
@@ -19,6 +24,16 @@ type JWTAuthMiddleware struct {
 // Handle implements the JWT validation over incoming request
 func (j *JWTAuthMiddleware) Handle(next func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// For faster development allowed to skip token authentication in DEBUG mode
+		if os.Getenv("DEBUG") != "" {
+			debugToken := regexp.MustCompile(`DebugToken (.*)`).FindStringSubmatch(r.Header.Get("Authorization"))
+			if len(debugToken) > 1 {
+				log.Debugf("Debug token, setting user to %v\n", debugToken[1])
+				r = r.WithContext(context.WithValue(r.Context(), types.ContextUserKey, debugToken[1]))
+				next(w, r)
+				return
+			}
+		}
 		client := auth0.NewJWKClient(auth0.JWKClientOptions{URI: fmt.Sprintf("https://%s/.well-known/jwks.json", j.auth0Domain)}, nil)
 		audience := j.auth0Audience
 		configuration := auth0.NewConfiguration(client, []string{audience}, fmt.Sprintf("https://%s/", j.auth0Domain), jose.RS256)
@@ -26,8 +41,7 @@ func (j *JWTAuthMiddleware) Handle(next func(w http.ResponseWriter, r *http.Requ
 
 		token, err := validator.ValidateRequest(r)
 		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Token is not valid:", token)
+			log.Errorf("Token %v is invalid, %v\n", token, err)
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte("Unauthorized"))
 		} else {
